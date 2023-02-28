4339f03481109ae5ab3e4153cadaeac855bfcaf9
mattorres90 <matheus@coliseum.gg>
2023-02-28 17:53:27 -0300

    refac: move files

--- src/components/Home/schema.ts
+++
@@ -1,7 +0,0 @@
-import { boolean, number, object, string } from "yup";
-
-export const schema = object({
-  name: string().required("É obrigatório escrever uma mensagem."),
-  chapters: number().nullable(),
-  isFinished: boolean(),
-});
