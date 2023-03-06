bb9bf41e18bc588d8fa9d87c914c6b80db06b09c
Matheus Torres <matheus@coliseum.gg>
2023-03-06 16:58:51 -0300

    refac: theme provider and protectedRoute admin verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,83 @@
-import { CircularProgress } from '@mui/material';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
+import { CircularProgress, createTheme, ThemeProvider } from '@mui/material';
+import { doc, getDoc, } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Route, Routes } from 'react-router'
 import { BrowserRouter, Navigate } from 'react-router-dom'
 import { CreateMangas } from './pages/Admin/AddManga';
 import { Home } from './pages/Home/Home';
 import Login from './pages/Login/Login'
+import { MangaList } from './pages/MangaList/MangaList';
 import Signup from './pages/Signup/Signup';
-import { auth } from './utils/firebaseUtils';
+import { auth, db } from './utils/firebaseUtils';
 
 export type ProtectedRouteProps = {
   isAuthenticated?: boolean;
   children: React.ReactElement;
   loading: boolean;
+  redirect?: string;
 }
 
 export const ProtectedRoute = ({
   isAuthenticated = false,
   loading,
+  redirect = '/',
   children
 }: ProtectedRouteProps): JSX.Element => {
   if (loading) return <CircularProgress style={{ position: 'absolute', top: 'calc(50% - 50px)', left: 'calc(50% - 50px)' }} size='100px' />
-  return !loading && isAuthenticated ? children : <Navigate to='/' />
+  return !loading && isAuthenticated ? children : <Navigate to={redirect} />
 }
 
+const theme = createTheme({
+  typography: {
+    fontFamily:
+      'Rubik'
+  }
+})
 
 function App() {
   const [user, loading] = useAuthState(auth)
+  const [isAdmin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    const transactionsRef = doc(db, `users/${user?.uid}`);
+    const docSnap = getDoc(transactionsRef);
+    docSnap.then((value) => {
+      setAdmin(value.get('role') === 'admin')
+    }
+    )
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/panel' element={
-          <ProtectedRoute isAuthenticated={!!user} loading={loading}>
-            <CreateMangas />
-          </ProtectedRoute>
-        } />
-        <Route path='/home' element={
-          <ProtectedRoute loading={loading} isAuthenticated={!!user}>
-            <Home />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={
+            <ProtectedRoute isAuthenticated={!user} redirect='/home' loading={loading}>
+              <Login />
+            </ProtectedRoute>} />
+          <Route path='/signup' element={
+            <ProtectedRoute isAuthenticated={!user} redirect='/home' loading={loading}>
+              <Signup />
+            </ProtectedRoute>} />
+          <Route path='/panel' element={
+            <ProtectedRoute isAuthenticated={isAdmin} redirect={!!user ? '/home' : '/'} loading={loading}>
+              <CreateMangas />
+            </ProtectedRoute>
+          } />
+          <Route path='/home' element={
+            <ProtectedRoute loading={loading} isAuthenticated={!!user}>
+              <Home />
+            </ProtectedRoute>
+          } />
+          <Route path='/mangas-list' element={
+            <ProtectedRoute loading={loading} isAuthenticated={!!user}>
+              <MangaList />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
