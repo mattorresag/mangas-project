0ac2b03d09e2ce700f034fc18c8e9e854ecb161b
Matheus Torres <matheus@coliseum.gg>
2023-03-06 16:59:57 -0300

    feat: access users mangas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,21 @@
-import { Typography } from '@mui/material';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { Button, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { collection, onSnapshot, query } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Flex } from '../../components/Flex';
 import { Header } from '../../components/Header';
 import { Manga } from '../../types/manga';
-import { db } from '../../utils/firebaseUtils';
+import { auth, db } from '../../utils/firebaseUtils';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = (): JSX.Element => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   const [mangas, setMangas] = useState<Manga[]>([])
 
   useEffect(() => {
-    const transactionsRef = collection(db, `mangas`);
+    const transactionsRef = collection(db, `user/${user?.uid}/mangas`);
     const q = query(transactionsRef);
     const unsub = onSnapshot(
       q,
@@ -35,13 +40,21 @@ export const Home = (): JSX.Element => {
     <Flex direction='column' align='center' justify='center' css={{ width: '100vw', height: 'calc(100vh - 16px)', maxWidth: '100%', gap: '32px' }}>
       <Header />
       <Typography variant='h2' color='#0092EE'>
-        Mangás
+        Meus Mangás
       </Typography>
       {mangas.map((manga) => (
         <Typography>
           {manga.name}
         </Typography>)
       )}
+      <Button variant='contained' onClick={() => navigate('/mangas-list')}>
+        <Flex align='center' css={{ gap: '8px' }}>
+          <AddIcon />
+          <Typography>
+            Adicionar novo mangá
+          </Typography>
+        </Flex>
+      </Button>
     </Flex>
   );
 }
