2d89686179e08959cc6eb79197386ad3aa58cfbd
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-07 17:37:25 -0300

    feat: manganato_id obrigatory when adding manga

--- src/pages/Admin/schema.ts
+++ src/pages/Admin/schema.ts
@@ -2,6 +2,9 @@ import { boolean, number, object, string } from "yup";
 
 export const schema = object({
   name: string().required("É obrigatório escrever uma mensagem."),
+  manganato_id: string().required(
+    "É obrigatório o ID do mangá do site Manganato."
+  ),
   chapters: number().nullable(),
   isFinished: boolean(),
 });
