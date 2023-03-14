7befc213e2d2e1adbb9871aa5d84038c78a80d6f
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-14 13:38:23 -0300

    refac: use mangaListHeader

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
-import { CircularProgress, Typography } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { useContext } from 'react';
-import { Flex } from '../../components/Flex';
-import { Header } from '../../components/Header';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../../provider/userProvider';
-import { StyledTypography } from '../../ui/Typography';
-import { StyledButton } from '../../ui/Button';
-import { Layout } from '../../ui/Layout';
-import { MyMangaItem } from '../../components/Manga/MyMangaItem';
+import { CircularProgress, Typography } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { useContext } from "react";
+import { Flex } from "../../components/Flex";
+import { Header } from "../../components/Header";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../provider/userProvider";
+import { StyledTypography } from "../../ui/Typography";
+import { StyledButton } from "../../ui/Button";
+import { Layout } from "../../ui/Layout";
+import { MangaItem } from "../../components/Manga/MangaItem";
+import { MangaListHeader } from "../../components/Manga/MangaListHeader";
 
 export const Home = (): JSX.Element => {
   const { currentUser, loading } = useContext(UserContext);
@@ -18,50 +19,35 @@ export const Home = (): JSX.Element => {
   return (
     <Layout>
       <Header />
-      <StyledTypography variant='h2'>
-        Meus Mangás
-      </StyledTypography>
+      <StyledTypography variant="h2">Meus Mangás</StyledTypography>
       {loading ? (
-        <CircularProgress size='100px' />
+        <CircularProgress size="100px" />
       ) : (
-        <Flex direction='column' css={{
-          width: '60vw', gap: '16px', maxHeight: '50vh', overflow: 'auto', position: 'relative', padding: '16px',
-        }}>
-          <Flex align='center' justify='between' css={{ gap: '8px', width: '100%', padding: '16px', position: 'sticky', top: '0', left: '0', background: '#3d5a80', zIndex: 10 }}>
-            <Flex align='center' css={{ gap: '16px', width: '30%' }}>
-              <Typography color='#202632'>
-                <strong>Capa</strong>
-              </Typography>
-              <Typography color='#202632'>
-                <strong>Nome do mangá</strong>
-              </Typography>
-            </Flex>
-            <Typography color='#202632'>
-              <strong>Último capítulo lido</strong>
-            </Typography>
-            <Typography color='#202632'>
-              <strong>Número de capítulos</strong>
-            </Typography>
-            <Typography color='#202632'>
-              <strong>Status</strong>
-            </Typography>
-          </Flex>
-          <Flex direction='column' >
+        <>
+          <MangaListHeader>
             {mangas?.map((manga) => (
-              <Flex css={{ background: '#98c1d9 ', '&:first-child': { marginTop: '-16px' }, '&:nth-child(odd)': { background: '#6b8ead' } }}>
-                <MyMangaItem manga={manga} />
+              <Flex
+                css={{
+                  background: "#98c1d9 ",
+                  "&:first-child": { marginTop: "-16px" },
+                  "&:nth-child(odd)": { background: "#6b8ead" },
+                }}
+              >
+                <MangaItem manga={manga} />
               </Flex>
             ))}
-          </Flex>
-        </Flex>)}
-      <StyledButton variant='contained' onClick={() => navigate('/mangas-list')}>
-        <Flex align='center' css={{ gap: '8px' }}>
-          <AddIcon />
-          <Typography>
-            Adicionar novo mangá
-          </Typography>
-        </Flex>
-      </StyledButton>
+          </MangaListHeader>
+          <StyledButton
+            variant="contained"
+            onClick={() => navigate("/mangas-list")}
+          >
+            <Flex align="center" css={{ gap: "8px" }}>
+              <AddIcon />
+              <Typography>Adicionar novo mangá</Typography>
+            </Flex>
+          </StyledButton>
+        </>
+      )}
     </Layout>
   );
-}
+};
--- src/pages/MangaList/MangaList.tsx
+++ src/pages/MangaList/MangaList.tsx
@@ -1,41 +1,44 @@
-import { Typography, TextField, Button } from '@mui/material';
-import { addDoc, collection, onSnapshot, query } from 'firebase/firestore';
-import { useContext, useEffect, useState } from 'react';
-import { Flex } from '../../components/Flex';
-import { Header } from '../../components/Header';
-import { Manga } from '../../types/manga';
-import { db } from '../../utils/firebaseUtils';
-import { MangaItem } from '../../components/Manga/MangaItem';
-import Dialog from '@mui/material/Dialog/Dialog';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form'
-import { InferType, number, object } from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { UserContext } from '../../provider/userProvider';
-import { StyledTypography } from '../../ui/Typography';
-import { Layout } from '../../ui/Layout';
-import { StyledButton } from '../../ui/Button';
+import { Typography, TextField, Button } from "@mui/material";
+import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { Flex } from "../../components/Flex";
+import { Header } from "../../components/Header";
+import { Manga } from "../../types/manga";
+import { db } from "../../utils/firebaseUtils";
+import { MangaItem } from "../../components/Manga/MangaItem";
+import Dialog from "@mui/material/Dialog/Dialog";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { InferType, number, object } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { UserContext } from "../../provider/userProvider";
+import { StyledTypography } from "../../ui/Typography";
+import { Layout } from "../../ui/Layout";
+import { MangaListHeader } from "../../components/Manga/MangaListHeader";
 
 const schema = object({
   lastRead: number()
-    .typeError('Deve ser um número.')
+    .typeError("Deve ser um número.")
     .required("É obrigatório inserir o último capítulo lido"),
 });
-
 
 type IFormValues = InferType<typeof schema>;
 
 export const MangaList = (): JSX.Element => {
   const { currentUser } = useContext(UserContext);
-  const [mangas, setMangas] = useState<Manga[]>([])
+  const [mangas, setMangas] = useState<Manga[]>([]);
   const [selectedManga, setSelectedManga] = useState<Manga | null>();
 
   const handleSelectManga = (manga: Manga | null) => {
-    setSelectedManga(manga)
-  }
+    setSelectedManga(manga);
+  };
 
-  const { handleSubmit, control, formState: { errors } } = useForm<IFormValues>({
-    resolver: yupResolver(schema)
-  })
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<IFormValues>({
+    resolver: yupResolver(schema),
+  });
 
   useEffect(() => {
     const transactionsRef = collection(db, `mangas`);
@@ -55,73 +58,79 @@ export const MangaList = (): JSX.Element => {
     );
   }, []);
 
-
   const submit: SubmitHandler<IFormValues> = (data) => {
-    console.log(data)
+    console.log(data);
     if (data) {
       addDoc(collection(db, `users/${currentUser?.uid}/mangas`), {
         name: selectedManga?.name,
         id: selectedManga?.id,
-        cover: selectedManga?.cover || '',
+        cover: selectedManga?.cover || "",
         chapter: selectedManga?.chapters,
-        lastRead: data.lastRead
-      }).then(() => alert('foi')).catch(() => alert('deu bico'))
+        lastRead: data.lastRead,
+      })
+        .then(() => alert("foi"))
+        .catch(() => alert("deu bico"));
     }
-  }
+  };
 
   return (
     <>
       <Layout>
         <Header />
-        <StyledTypography variant='h2'>
-          Mangás
-        </StyledTypography>
-        <Flex direction='column' css={{ width: '60vw', gap: '16px', maxHeight: '50vh', overflow: 'auto', position: 'relative' }}>
-          <Flex align='center' justify='between' css={{ gap: '8px', width: '100%', position: 'sticky', top: '0', left: '0', background: 'red', zIndex: 10 }}>
-            <Flex align='center' css={{ gap: '16px', width: '30%' }}>
-              <Typography fontFamily='Rubik'>
-                Capa
-              </Typography>
-              <Typography>
-                Nome do mangá
-              </Typography>
-            </Flex>
-            <Typography fontFamily='Rubik'>
-              Número de capítulos
-            </Typography>
-            <Typography fontFamily='Rubik'>
-              Status
-            </Typography>
-          </Flex>
+        <StyledTypography variant="h2">Mangás</StyledTypography>
+        <MangaListHeader>
           {mangas.map((manga) => (
-            <MangaItem handleSelectManga={handleSelectManga} manga={manga} />
+            <Flex
+              css={{
+                background: "#98c1d9 ",
+                "&:first-child": { marginTop: "-16px" },
+                "&:nth-child(odd)": { background: "#6b8ead" },
+              }}
+            >
+              <MangaItem
+                isCRUD
+                handleSelectManga={handleSelectManga}
+                manga={manga}
+              />
+            </Flex>
           ))}
-        </Flex>
+        </MangaListHeader>
       </Layout>
       <Dialog onClose={() => setSelectedManga(null)} open={!!selectedManga}>
-        <Flex direction='column' align='center' css={{ borderRadius: '16px', width: '30vw', height: '100%', gap: '16px', padding: '8px' }}>
-          <StyledTypography variant='h4'>
+        <Flex
+          direction="column"
+          align="center"
+          css={{
+            borderRadius: "16px",
+            width: "30vw",
+            height: "100%",
+            gap: "16px",
+            padding: "8px",
+          }}
+        >
+          <StyledTypography variant="h4">
             {selectedManga?.name}
           </StyledTypography>
           <form onSubmit={handleSubmit(submit)}>
-            <Flex direction='column' css={{ gap: '16px', width: '100%' }}>
-              <Flex css={{ width: '100%' }}>
+            <Flex direction="column" css={{ gap: "16px", width: "100%" }}>
+              <Flex css={{ width: "100%" }}>
                 <Controller
                   control={control}
-                  name='lastRead'
+                  name="lastRead"
                   render={({ field }) => (
                     <TextField
-                      style={{ width: '100%' }}
+                      style={{ width: "100%" }}
                       type="text"
                       placeholder="Último capítulo lido"
                       error={!!errors.lastRead?.message}
                       helperText={errors.lastRead?.message}
                       {...field}
                     />
-                  )} />
+                  )}
+                />
               </Flex>
-              <Flex css={{ width: '100%' }}>
-                <Button type='submit' variant='contained'>
+              <Flex css={{ width: "100%" }}>
+                <Button type="submit" variant="contained">
                   Adicionar mangá a minha lista
                 </Button>
               </Flex>
@@ -131,4 +140,4 @@ export const MangaList = (): JSX.Element => {
       </Dialog>
     </>
   );
-}
+};
