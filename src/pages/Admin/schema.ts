81b6d0ccb9461855b643db9e16a4c9966463b61c
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-14 13:38:01 -0300

    feat: cover insertion

--- src/pages/Admin/schema.ts
+++ src/pages/Admin/schema.ts
@@ -2,6 +2,7 @@ import { boolean, number, object, string } from "yup";
 
 export const schema = object({
   name: string().required("É obrigatório escrever uma mensagem."),
+  cover: string().required("É obrigatório inserir uma capa."),
   manganato_id: string().required(
     "É obrigatório o ID do mangá do site Manganato."
   ),
