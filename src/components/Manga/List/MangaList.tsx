331dd195cf6e94cbd4be97297b6633f2b6b8e6f1
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-29 11:59:31 -0300

    feat: search for mangas

--- src/components/Manga/List/MangaList.tsx
+++ src/components/Manga/List/MangaList.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { TextField } from "@mui/material";
+import React, { ChangeEvent, useState } from "react";
+import { useDebounce } from "react-use";
 import createBreakpoint from "../../../hooks/useWindowSize";
 import { Manga } from "../../../types/manga";
 import { Flex } from "../../Flex";
@@ -17,14 +19,40 @@ export const MangaList = ({ mangas, isHome = false }: Props): JSX.Element => {
   const breakpoint = useBreakpoint();
   const isMobile = ["xs", "xxs"].includes(breakpoint);
   const [selectedManga, setSelectedManga] = useState<Manga | null>();
+  const [debounced, setDebounced] = useState("");
+
+  const [userSearch, setUserSearch] = useState("");
+
+  const filteredMangas =
+    mangas && debounced.length > 0
+      ? mangas.filter((manga) => manga.name.toLowerCase().includes(debounced))
+      : mangas;
 
   const handleSelectManga = (manga: Manga | null) => {
     setSelectedManga(manga);
   };
+
+  useDebounce(
+    () => {
+      setDebounced(userSearch || "");
+    },
+    300,
+    [userSearch]
+  );
+
   return (
-    <>
+    <Flex direction="column" css={{ gap: "16px" }}>
+      <Flex justify={isMobile ? "center" : "end"}>
+        <TextField
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            const { value } = e.target;
+            setUserSearch(value.toLowerCase());
+          }}
+          placeholder="Buscar Mangá"
+        />
+      </Flex>
       <MangaListHeader isCRUD={!isHome}>
-        {mangas?.map((manga) => (
+        {filteredMangas?.map((manga) => (
           <Flex
             css={{
               background: "rgb(152, 193, 217, 0.2)",
@@ -52,6 +80,6 @@ export const MangaList = ({ mangas, isHome = false }: Props): JSX.Element => {
         selectedManga={selectedManga}
         isEditing={isHome}
       />
-    </>
+    </Flex>
   );
 };
