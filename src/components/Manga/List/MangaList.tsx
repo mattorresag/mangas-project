c880fdd32b7cf166775f2ff374acb2f0b67a3c62
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-29 11:51:47 -0300

    refac: import mangaItems

--- src/components/Manga/List/MangaList.tsx
+++ src/components/Manga/List/MangaList.tsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
+import createBreakpoint from "../../../hooks/useWindowSize";
 import { Manga } from "../../../types/manga";
 import { Flex } from "../../Flex";
 import { AddManga } from "../AddManga";
-import { MangaItem } from "./MangaItem";
+import { MangaItemDesktop } from "./Item/MangaItemDesktop";
+import { MangaItemMobile } from "./Item/MangaItemMobile";
 import { MangaListHeader } from "./MangaListHeader";
 interface Props {
   mangas?: Manga[];
   isHome?: boolean;
 }
+
+const useBreakpoint = createBreakpoint();
+
 export const MangaList = ({ mangas, isHome = false }: Props): JSX.Element => {
+  const breakpoint = useBreakpoint();
+  const isMobile = ["xs", "xxs"].includes(breakpoint);
   const [selectedManga, setSelectedManga] = useState<Manga | null>();
 
   const handleSelectManga = (manga: Manga | null) => {
@@ -24,11 +31,19 @@ export const MangaList = ({ mangas, isHome = false }: Props): JSX.Element => {
               "&:nth-child(odd)": { background: "rgb(107, 142, 173,0.4)" },
             }}
           >
-            <MangaItem
-              isCRUD={!isHome}
-              manga={manga}
-              handleSelectedManga={handleSelectManga}
-            />
+            {isMobile ? (
+              <MangaItemMobile
+                isCRUD={!isHome}
+                manga={manga}
+                handleSelectedManga={handleSelectManga}
+              />
+            ) : (
+              <MangaItemDesktop
+                isCRUD={!isHome}
+                manga={manga}
+                handleSelectedManga={handleSelectManga}
+              />
+            )}
           </Flex>
         ))}
       </MangaListHeader>
