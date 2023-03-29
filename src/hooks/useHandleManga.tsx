1c8062bc8974319d7a7b99caed62e1a04c51a1b4
Matheus Torres <matheustorres@poli.ufrj.br>
2023-03-29 11:40:48 -0300

    refac: move manga handlers to useHandleManga hook

--- src/hooks/useHandleManga.tsx
+++ src/hooks/useHandleManga.tsx
@@ -1,18 +1,21 @@
-import { doc, setDoc, updateDoc } from "firebase/firestore";
-import React, { useContext } from "react";
+import axios from "axios";
+import { deleteDoc, doc, setDoc, updateDoc } from "firebase/firestore";
+import React, { useContext, useState } from "react";
 import { toast } from "react-toastify";
 import { UserContext } from "../provider/userProvider";
 import { Manga } from "../types/manga";
 import { db } from "../utils/firebaseUtils";
 interface Props {
   selectedManga?: Manga | null;
-  handleSelectedManga: (manga: Manga | null) => void;
+  handleSelectedManga?: (manga: Manga | null) => void;
 }
 export const useHandleManga = ({
   selectedManga,
   handleSelectedManga,
 }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useContext(UserContext);
+
   const handleAddManga = ({ lastRead }: { lastRead: number }) => {
     const isDuplicate = currentUser?.mangas?.find(
       (userManga) => userManga.name === selectedManga?.name
@@ -47,7 +50,7 @@ export const useHandleManga = ({
               }
             )
           )
-          .finally(() => handleSelectedManga(null));
+          .finally(() => handleSelectedManga?.(null));
   };
 
   const handleUpdateManga = ({ lastRead }: { lastRead: number }) => {
@@ -63,10 +66,92 @@ export const useHandleManga = ({
           type: "error",
         })
       )
-      .finally(() => handleSelectedManga(null));
+      .finally(() => handleSelectedManga?.(null));
   };
+
+  const handleDeleteManga = ({
+    manga,
+    isUserManga = true,
+  }: {
+    manga: Manga;
+    isUserManga?: boolean;
+  }) => {
+    const deleteFromUserList = deleteDoc(
+      doc(db, `users/${currentUser?.uid}/mangas/${manga.name}`)
+    )
+      .then(() =>
+        toast(`O mangá ${manga.name} foi deletado da sua lista com sucesso!`)
+      )
+      .catch(() =>
+        toast(`Ocorreu um erro ao deletar o mangá ${manga.name} da sua lista.`)
+      )
+      .finally(() => setIsLoading(false));
+
+    const deleteFromMangaList = deleteDoc(doc(db, `mangas/${manga.name}`))
+      .then(() => toast(`O mangá ${manga.name} foi deletado com sucesso!`))
+      .catch(() => toast(`Ocorreu um erro ao deletar o mangá ${manga.name}.`))
+      .finally(() => setIsLoading(false));
+
+    if (window.confirm(`Deseja realmente deletar o mangá ${manga.name}?`)) {
+      setIsLoading(true);
+      isUserManga ? deleteFromUserList : deleteFromMangaList;
+    }
+  };
+
+  const handleUpdateLastChapter = ({ manga }: { manga: Manga }) => {
+    axios
+      .get(
+        "https://corsproxy.io/?" +
+          encodeURIComponent(
+            `https://api.mangadex.org/manga/${manga.mangadex_id}/feed?order[chapter]=desc`
+          ),
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(
+        (data: {
+          data: {
+            data: {
+              attributes: {
+                chapter: string;
+              };
+            }[];
+          };
+        }) => {
+          const newChapter = Number(
+            data?.data?.data?.[0]?.attributes?.chapter || 0
+          );
+          manga.chapters !== newChapter
+            ? updateDoc(
+                doc(db, `users/${currentUser?.uid}/mangas/${manga?.name}`),
+                {
+                  chapters: newChapter,
+                }
+              )
+                .then(() =>
+                  toast(
+                    `Último capítulo do mangá ${manga?.name} atualizado com sucesso!`
+                  )
+                )
+                .catch(() =>
+                  toast(
+                    `Ocorreu um erro ao atualizar o último capítulo do mangá ${manga?.name} `
+                  )
+                )
+            : toast("O capítulo já está atualizado!");
+        }
+      );
+  };
+
   return {
     handleUpdateManga,
     handleAddManga,
+    handleUpdateLastChapter,
+    handleDeleteManga,
+    isLoading,
   };
 };
